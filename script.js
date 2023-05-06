2e44ee9dc899541b265f2f13b4c2c4a250c6b113
Jeffrey Raj <jeffreyrajcj@gmail.com>
2023-05-06 19:49:19 +0530

    added logic to run each game and display results using DOM

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
     const rockButton= document.querySelector("#rockButton");
     const paperButton = document.querySelector("#paperButton");
     const scissorButton = document.querySelector("#scissorButton");
+    const resultContainer= document.querySelector("#resultContainer");
+    const resultDiv= document.createElement('div');
+    let computerWins=0;
+    let playerWins=0;
     rockButton.addEventListener("click", ()=>{
         let userChoice='rock';
         playGame(userChoice)});
@@ -8,16 +12,70 @@
         let userChoice='paper';
         playGame(userChoice)});
     scissorButton.addEventListener("click", ()=>{
-        let userChoice='scissor';
+        let userChoice='scissors';
         playGame(userChoice)});
     
 
     
     function playGame(userChoice){
-        let computerSelection= getComputerChoice();
+        let computerChoice= getComputerChoice();
         //let userChoice = num;
         console.log(userChoice);
-        console.log(computerSelection);
+        console.log(computerChoice);
+        if(userChoice==computerChoice){
+            resultDiv.textContent=`It is a tie. Both chose ${userChoice}`;
+            resultContainer.append(resultDiv);
+
+        }
+        if(userChoice=="rock")
+        {
+
+            if(computerChoice=="paper")
+            {
+                resultDiv.textContent=`Computer wins!! You chose ${userChoice} Computer chose ${computerChoice}`;
+                resultContainer.append(resultDiv);
+                computerWins++;
+            }
+            if(computerChoice=="scissors")
+            {
+                resultDiv.textContent=`Player Wins!! You chose ${userChoice} Computer chose ${computerChoice}`;
+                resultContainer.append(resultDiv);
+                playerWins++;
+            }
+        }
+        if(userChoice=="paper")
+        {
+
+            if(computerChoice=="rock")
+            {
+                resultDiv.textContent=`Player Wins!! You chose ${userChoice} Computer chose ${computerChoice}`;
+                resultContainer.append(resultDiv);
+                playerWins++;
+            }
+            if(computerChoice=="scissors")
+            {
+                resultDiv.textContent=`Computer wins!! You chose ${userChoice} Computer chose ${computerChoice}`;
+                resultContainer.append(resultDiv);
+                computerWins++;
+            }
+        }
+        if(userChoice=="scissors")
+        {
+
+            if(computerChoice=="rock")
+            {
+                resultDiv.textContent=`Computer wins!! You chose ${userChoice} Computer chose ${computerChoice}`;
+                resultContainer.append(resultDiv);
+                computerWins++;
+            }
+            if(computerChoice=="paper")
+            {
+                resultDiv.textContent=`Player Wins!! You chose ${userChoice} Computer chose ${computerChoice}`;
+                resultContainer.append(resultDiv);
+                playerWins++;
+            }
+        }
+        
 
     }
         function getComputerChoice(){
